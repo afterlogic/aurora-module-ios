bcb28ca6c45132d248f2b50bf7249e8bce685ccf
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:49:13 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\Ios;
 
-class IosModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	/***** private functions *****/
 	/**
