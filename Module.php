5d32af5bcc6f257d2878ae48fe6d6fc3d3a9d82f
sash04ek <a.ovcharov@gmail.com>
2018-11-19 16:20:33 +0300

    global refactoring

--- Module.php
+++ Module.php
@@ -24,8 +24,6 @@ class Module extends \Aurora\System\Module\AbstractModule
 	 */
 	public function init() 
 	{
-		parent::init();
-		
 		$this->AddEntries(array(
 				'ios' => 'EntryIos',
 				'profile' => 'EntryProfile'
