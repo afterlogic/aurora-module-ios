4154917ccc6c28e47145d3be26c01830f7f02e10
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:45:01 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\Ios;
 
 /**
+ * Allows to configure iOS device automatically for syncing mails, contacts and calendars using iOS profile.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
